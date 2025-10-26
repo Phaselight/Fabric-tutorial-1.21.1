4c0f36b289b035eda6f742fe476cabc086a7b8e2
Phase <phaselight1001@gmail.com>
2025-10-26 19:59:35 -0400

    Added Item Class

---
+++ src/main/java/phase/tutorialmod/item/ModItems.java
@@ -0,0 +1,15 @@
+package phase.tutorialmod.item;
+
+import phase.tutorialmod.TutorialMod;
+
+public class ModItems {
+
+
+
+    public static void registerModItems() {
+        TutorialMod.LOGGER.info("Registering ModItems for " + TutorialMod.MOD_ID);
+    }
+
+
+
+}
