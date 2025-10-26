fa5ab450a1aab374fadc120ab369000361341c51
Phase <phaselight1001@gmail.com>
2025-10-26 19:51:55 -0400

    First Commit

---
+++ src/main/java/phase/tutorialmod/TutorialModClientClass.java
@@ -0,0 +1,12 @@
+package phase.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+// Very important comments
+public class TutorialModClientClass implements ClientModInitializer{
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
