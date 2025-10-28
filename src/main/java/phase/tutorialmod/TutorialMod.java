9cc70d939f39c3038090282e9e1baf58d2f4aa0f
Phase <phaselight1001@gmail.com>
2025-10-28 19:08:38 -0400

    Added two creative mode tabs to Minecraft

--- src/main/java/phase/tutorialmod/TutorialMod.java
+++ src/main/java/phase/tutorialmod/TutorialMod.java
@@ -5,17 +5,20 @@ import net.fabricmc.api.ModInitializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import phase.tutorialmod.block.ModBlocks;
+import phase.tutorialmod.item.ModItemGroups;
 import phase.tutorialmod.item.ModItems;
 
 public class TutorialMod implements ModInitializer {
 	public static final String MOD_ID = "tutorialmod";
-
 	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
 	@Override
 	public void onInitialize() {
+        ModItemGroups.registerItemGroups();
+
         ModItems.registerModItems();
         ModBlocks.registerModBlocks();
+
 
 	}
 }
